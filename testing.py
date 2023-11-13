8a41966de01612d2f734894f377e05325c162345
Abhishek Verma <115094713+Abhishek-Verma75230@users.noreply.github.com>
2023-11-14 03:37:13 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,38 @@
-#writing all codes
+#!/usr/bin/env python3
+
+# ... [rest of the import statements]
+
+# Assuming swift_msgs.msg contains a valid message type for drone commands
+from swift_msgs.msg import SwiftCommand
+
+class Camera:
+
+    def __init__(self):
+        # ... [rest of the initialization]
+
+        # Replace swift_msgs with the correct message type
+        self.cmd = SwiftCommand()
+        # ... [rest of the command initialization]
+
+        # ... [rest of the publisher and subscriber initialization]
+
+        self.arm()
+
+    # ... [rest of the methods]
+
+    def callback(self, data):
+        # ... [rest of the image processing code]
+
+        # Comment out if running on a headless setup
+        # cv2.imshow('Image', gray)
+        # cv2.waitKey(0)
+
+        # ... [rest of the callback method]
+
+    # ... [rest of the methods including detect_and_land if applicable]
+
+if __name__ == '__main__':
+    try:
+        swift_drone = Camera()
+        r = rospy.Rate(30)
+        while
